6c4a5f6b0bfe3b4c8f913195840ca3c9138d8847
Howardzhangdqs <3504575710@qq.com>
2022-10-27 21:09:02 +0800

    feat(client): 新增查询功能

--- client/decode.js
+++ client/decode.js
@@ -1,8 +1,6 @@
 const G = require("./Gzip.js");
 const chalk = require("chalk");
 const fs = require("fs");
-data = JSON.parse(G.decode.file("./filelib/.md5"));
-okf = fs.readdirSync("./filelib/");
 
 const readline = require('readline');
 const rl = readline.createInterface({
@@ -29,12 +27,42 @@ var format_date = function (t) {
            ('000' + t.getMilliseconds()).slice(-4));
 };
 
+var data = JSON.parse(G.decode.file("./filelib/.md5"));
+var filelib = fs.readdirSync("./filelib/");
+var _flib = [];
+const cmd = {
+	"log": () => {
+		for (let i in _flib)
+			console.log(chalk.green(_flib[i].m),
+				format_date(new Date(_flib[i].t)),
+				chalk.cyan(_flib[i].n));
+	},
+};
+
 (async function () {
 	for (let i in data)
-		if (okf.indexOf(i) >= 0)
-			console.log(chalk.green(i), format_date(new Date(data[i].t)), chalk.cyan(data[i].p.split(/[\\/]/g).pop()));
+		if (filelib.indexOf(i) >= 0)
+			_flib.push({
+				m: i,
+				t: data[i].t,
+				n: data[i].p.split(/[\\/]/g).pop(),
+			});
+			
 
-	await question("");
+	_flib.sort((a, b) => (b.t - a.t));
+
+	cmd.log();
+
+	let req = await question("\n> ");
+	while (req != "") {
+
+		if (cmd[req]) cmd[req]();
+		else console.log("< " + chalk.red(`Command \`${chalk.cyan(req)}\` not found`));
+
+		req = await question("> ");
+	}
+	
+	console.log(chalk.red("\nExit"));
+
 	process.exit(0);
 })();
-
