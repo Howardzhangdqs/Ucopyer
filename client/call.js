70422cc4ddad25991fb538d81002422b7f087cc4
Howard Zhang <75195784+Howardzhangdqs@users.noreply.github.com>
2022-10-30 08:41:15 +0800

    feat(*): myshortcut

--- client/call.js
+++ client/call.js
@@ -1,8 +1,22 @@
-const cmd = require('node-cmd');
+const cmd = require("node-cmd");
 
+//cmd.run("ucopyer");
+
+const fs = require("fs");
+const path = require("path");
+
+const dirls = ["Microsoft/Windows/Recent", "Microsoft/Office/Recent"];
+
+for (let i in dirls) {
+	let Recentpth = path.join(process.env.APPDATA, dirls[i]);
+	fs.watch(Recentpth, (method, dir) => {
+		if (method == "change")
+			console.log(method, path.join(Recentpth, dir));
+	});
+}
+/*
 setInterval(function () {
 	console.log("Main process: calling python");
-	console.log(cmd.runSync('fetch'));
+	console.log(cmd.runSync("fetch"));
 }, 1000 * 60 * 5);
-
-cmd.run('ucopyer')
+*/
